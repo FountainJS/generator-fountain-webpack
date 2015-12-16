87f3f213954db643f793f23b8f4071888356d78e
Matthieu Lux <matthieu.lux@gmail.com>
2015-12-16 23:01:33 +0100

    test json conf with webpack

--- generators/app/index.js
+++ generators/app/index.js
@@ -39,11 +39,49 @@ module.exports = fountain.Base.extend({
     },
 
     conf: function () {
-      this.fs.copyTpl(
-        this.templatePath('conf'),
-        this.destinationPath('conf'),
-        { framework: this.props.framework }
-      );
+      const lit = this.lit;
+      const conf = {
+        debug: true,
+        devtool: '#eval-source-map',
+        output: {
+          path: lit`path.join(process.cwd(), conf.paths.tmp)`,
+          filename: 'index.js'
+        },
+        plugins: [
+          lit`new webpack.optimize.OccurenceOrderPlugin()`,
+          lit`new webpack.NoErrorsPlugin()`
+        ],
+        module: {
+          loaders: [{ test: lit`/\.js$/`, exclude: lit`/node_modules/` }]
+        }
+      };
+
+      if (this.props.framework === 'react') {
+        conf.entry = [
+          'webpack/hot/dev-server',
+          'webpack-hot-middleware/client',
+          lit`\`./\${path.join(conf.paths.src, 'index')}\``
+        ];
+      } else {
+        conf.entry = lit`\`./\${path.join(conf.paths.src, 'index')}\``;
+      }
+
+      if (this.props.framework === 'react') {
+        conf.plugins.push(
+          lit`new webpack.HotModuleReplacementPlugin()`
+        );
+      }
+
+      const loader = conf.module.loaders[0];
+      if (this.props.framework === 'react') {
+        loader.loaders = ['react-hot', 'babel'];
+      } else if (this.props.framework === 'angular1') {
+        loader.loaders = ['ng-annotate', 'babel'];
+      } else {
+        loader.loader = 'babel';
+      }
+
+      this.copyTemplate('conf/webpack.conf.js', { webpackConf: conf });
     }
   },
 

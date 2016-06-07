1aacb40360b6216b7d2371fcc8c3c44257343ccb
Delapouite <delapouite@gmail.com>
2016-06-07 16:26:16 +0200

    Add stylus support

--- generators/app/conf.js
+++ generators/app/conf.js
@@ -114,6 +114,10 @@ module.exports = function webpackConf(options) {
       cssLoaders.push('less');
       test = lit`/\\.(css|less)$/`;
     }
+    if (options.css === 'stylus') {
+      cssLoaders.push('stylus');
+      test = lit`/\\.(css|styl|stylus)$/`;
+    }
     cssLoaders.push('postcss');
     conf.module.loaders.push({test, loaders: cssLoaders});
   }

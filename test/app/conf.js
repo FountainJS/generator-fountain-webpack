4288c5c3ce0c7514e65d47c51c5336ab13888fc9
Micaël <micael.mbagira@icloud.com>
2016-06-28 16:48:14 +0200

    Add tests

---
+++ test/app/conf.js
@@ -0,0 +1,365 @@
+const test = require('ava');
+const _ = require('lodash');
+const lit = require('fountain-generator').lit;
+const webpackConf = require('../../generators/app/conf');
+
+function merge(args) {
+  const result = {};
+  _.mergeWith(result, ...args, (x, y) => {
+    if (_.isArray(x)) {
+      return _.uniq(x.concat(y));
+    }
+  });
+  return result;
+}
+
+const conf = {
+  module: {
+    loaders: [
+      {test: lit`/\.json$/`, loaders: ['json']}
+    ]
+  }
+};
+
+test(t => {
+  const options = {
+    test: false,
+    dist: false,
+    framework: 'react',
+    css: 'css',
+    js: 'babel'
+  };
+  const expected = merge([{}, conf, {
+    module: {
+      loaders: [
+        {
+          test: lit`/\\.css$/`,
+          loaders: ['style', 'css', 'postcss']
+        },
+        {
+          test: lit`/\\.js$/`,
+          exclude: lit`/node_modules/`,
+          loaders: ['react-hot', 'babel']}
+      ]
+    },
+    plugins: [
+      lit`new webpack.optimize.OccurrenceOrderPlugin()`,
+      lit`new webpack.NoErrorsPlugin()`,
+      lit`new HtmlWebpackPlugin({
+      template: conf.path.src('index.html'),
+      inject: true
+    })`,
+      lit`new webpack.HotModuleReplacementPlugin()`
+    ],
+    postcss: lit`() => [autoprefixer]`,
+    debug: true,
+    devtool: 'cheap-module-eval-source-map',
+    output: {
+      path: lit`path.join(process.cwd(), conf.paths.tmp)`,
+      filename: 'index.js'
+    },
+    entry: [
+      'webpack/hot/dev-server',
+      'webpack-hot-middleware/client',
+      lit`\`./\${conf.path.src('index')}\``
+    ]
+  }]);
+  const result = webpackConf(options);
+  t.deepEqual(result, expected);
+});
+
+test(t => {
+  const options = {
+    test: true,
+    dist: false,
+    framework: 'react',
+    css: 'css',
+    js: 'typescript'
+  };
+  const expected = merge([{}, conf, {
+    plugins: [],
+    module: {
+      loaders: [
+        {
+          test: lit`/\\.tsx$/`,
+          exclude: lit`/node_modules/`,
+          loaders: ['ts']
+        }
+      ]
+    },
+    debug: true,
+    devtool: 'cheap-module-eval-source-map',
+    resolve: {
+      extensions: ['', '.webpack.js', '.web.js', '.js', '.ts', '.tsx']
+    },
+    externals: lit`{
+    'jsdom': 'window',
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': 'window',
+    'text-encoding': 'window'
+  }`,
+    ts: {
+      configFileName: 'conf/ts.conf.json'
+    },
+    tslint: {
+      configuration: lit`require('../tslint.json')`
+    }
+  }]);
+  const result = webpackConf(options);
+  t.deepEqual(result, expected);
+});
+
+test('conf with angular1/scss/js', t => {
+  const options = {
+    test: false,
+    dist: true,
+    framework: 'angular1',
+    css: 'scss',
+    js: 'js'
+  };
+  const expected = merge([{}, conf, {
+    module: {
+      loaders: [
+        {
+          test: lit`/\\.(css|scss)$/`,
+          loaders: ['style', 'css', 'sass', 'postcss']
+        },
+        {
+          test: lit`/\\.js$/`,
+          exclude: lit`/node_modules/`,
+          loaders: ['ng-annotate']
+        }
+      ]
+    },
+    plugins: [
+      lit`new webpack.optimize.OccurrenceOrderPlugin()`,
+      lit`new webpack.NoErrorsPlugin()`,
+      lit`new HtmlWebpackPlugin({
+      template: conf.path.src('index.html'),
+      inject: true
+    })`,
+      lit`new webpack.optimize.UglifyJsPlugin({
+      compress: {unused: true, dead_code: true} // eslint-disable-line camelcase
+    })`
+    ],
+    postcss: lit`() => [autoprefixer]`,
+    output: {
+      path: lit`path.join(process.cwd(), conf.paths.dist)`,
+      filename: 'index-[hash].js'
+    },
+    entry: [
+      lit`\`./\${conf.path.src('index')}\``,
+      lit`\`./\${conf.path.tmp('templateCacheHtml.js')}\``
+    ]
+  }]);
+  const result = webpackConf(options);
+  t.deepEqual(result, expected);
+});
+
+test('conf with angular1/styl/typescript', t => {
+  const options = {
+    test: false,
+    dist: true,
+    framework: 'angular1',
+    css: 'styl',
+    js: 'typescript'
+  };
+  const expected = merge([{}, conf, {
+    module: {
+      loaders: [
+        {
+          test: lit`/\\.(css|styl|stylus)$/`,
+          loaders: ['style', 'css', 'stylus', 'postcss']
+        },
+        {
+          test: lit`/\\.ts$/`,
+          exclude: lit`/node_modules/`,
+          loaders: ['ng-annotate', 'ts']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['', '.webpack.js', '.web.js', '.js', '.ts']
+    },
+    plugins: [
+      lit`new webpack.optimize.OccurrenceOrderPlugin()`,
+      lit`new webpack.NoErrorsPlugin()`,
+      lit`new HtmlWebpackPlugin({
+      template: conf.path.src('index.html'),
+      inject: true
+    })`,
+      lit`new webpack.optimize.UglifyJsPlugin({
+      compress: {unused: true, dead_code: true} // eslint-disable-line camelcase
+    })`
+    ],
+    postcss: lit`() => [autoprefixer]`,
+    output: {
+      path: lit`path.join(process.cwd(), conf.paths.dist)`,
+      filename: 'index-[hash].js'
+    },
+    entry: [
+      lit`\`./\${conf.path.src('index')}\``,
+      lit`\`./\${conf.path.tmp('templateCacheHtml.ts')}\``
+    ]
+  }]);
+  const result = webpackConf(options);
+  t.deepEqual(result.module, expected.module);
+});
+
+test('conf with angular2/less/typescript', t => {
+  const options = {
+    test: false,
+    dist: true,
+    framework: 'angular2',
+    css: 'less',
+    js: 'typescript'
+  };
+  const expected = merge([{}, conf, {
+    module: {
+      loaders: [
+        {
+          test: lit`/\\.(css|less)$/`,
+          loaders: ['style', 'css', 'less', 'postcss']
+        },
+        {
+          test: lit`/\\.ts$/`,
+          exclude: lit`/node_modules/`,
+          loaders: ['ts']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['', '.webpack.js', '.web.js', '.js', '.ts']
+    },
+    plugins: [
+      lit`new webpack.optimize.OccurrenceOrderPlugin()`,
+      lit`new webpack.NoErrorsPlugin()`,
+      lit`new HtmlWebpackPlugin({
+      template: conf.path.src('index.html'),
+      inject: true
+    })`,
+      lit`new webpack.optimize.UglifyJsPlugin({
+      compress: {unused: true, dead_code: true} // eslint-disable-line camelcase
+    })`
+    ],
+    postcss: lit`() => [autoprefixer]`,
+    output: {
+      path: lit`path.join(process.cwd(), conf.paths.dist)`,
+      filename: 'index-[hash].js'
+    },
+    entry: [
+      lit`\`./\${conf.path.src('index')}\``
+    ]
+  }]);
+  const result = webpackConf(options);
+  t.deepEqual(result.module, expected.module);
+});
+
+test('conf with react/css/babel', t => {
+  const options = {
+    test: true,
+    dist: false,
+    framework: 'react',
+    css: 'css',
+    js: 'babel'
+  };
+  const expected = merge([{}, conf, {
+    plugins: [],
+    module: {
+      loaders: [
+        {
+          test: lit`/\\.js$/`,
+          exclude: lit`/node_modules/`,
+          loaders: ['babel']
+        },
+        {
+          test: lit`/\\.js$/`,
+          exclude: lit`/(node_modules|.*\\.spec\\.js)/`,
+          loader: 'isparta'
+        }
+      ]
+    },
+    debug: true,
+    devtool: 'cheap-module-eval-source-map',
+    externals: {
+      'react/lib/ExecutionEnvironment': true,
+      'react/lib/ReactContext': true
+    }
+  }]);
+  const result = webpackConf(options);
+  t.deepEqual(result.module, expected.module);
+});
+
+test('conf with angular2/css/babel', t => {
+  const options = {
+    test: true,
+    dist: false,
+    framework: 'angular2',
+    css: 'css',
+    js: 'babel'
+  };
+  const expected = merge([{}, conf, {
+    plugins: [],
+    debug: true,
+    devtool: 'cheap-module-eval-source-map',
+    module: {
+      loaders: [
+        {
+          test: lit`/\\.js$/`,
+          exclude: lit`/node_modules/`,
+          loaders: ['babel']
+        },
+        {
+          test: lit`/\\.js$/`,
+          exclude: lit`/(node_modules|.*\\.spec\\.js)/`,
+          loader: 'isparta'
+        }
+      ]
+    }
+  }]);
+  const result = webpackConf(options);
+  t.deepEqual(result.module, expected.module);
+});
+
+test('conf with angular2/css/js', t => {
+  const options = {
+    test: false,
+    dist: true,
+    framework: 'angular2',
+    css: 'css',
+    js: 'js'
+  };
+  const expected = merge([{}, conf, {
+    module: {
+      loaders: [
+        {
+          test: lit`/\\.css$/`,
+          loaders: ['style', 'css', 'postcss']
+        }
+      ]
+    },
+    plugins: [
+      lit`new webpack.optimize.OccurrenceOrderPlugin()`,
+      lit`new webpack.NoErrorsPlugin()`,
+      lit`new HtmlWebpackPlugin({
+      template: conf.path.src('index.html'),
+      inject: true
+    })`,
+      lit`new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    })`,
+      lit`new webpack.optimize.UglifyJsPlugin({
+    compress: {unused: true, dead_code: true} // eslint-disable-line camelcase
+  })`
+    ],
+    postcss: lit`() => [autoprefixer]`,
+    output: {
+      path: lit`path.join(process.cwd(), conf.paths.dist)`,
+      filename: 'index-[hash].js'
+    },
+    entry: lit`\`./\${conf.path.src('index')}\``
+  }]);
+  const result = webpackConf(options);
+  t.deepEqual(result.module, expected.module);
+});

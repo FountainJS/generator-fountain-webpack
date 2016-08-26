e78321e9bebe54b192e46635ef3e85eb7eac2ba0
Micaël <micael.mbagira@icloud.com>
2016-08-26 15:32:27 +0200

    Use NODE_ENV instead of BABEL_ENV and remove it for dev envs

--- generators/app/templates/gulp_tasks/webpack.js
+++ generators/app/templates/gulp_tasks/webpack.js
@@ -5,25 +5,20 @@ const webpack = require('webpack');
 const webpackConf = require('../conf/webpack.conf');
 const webpackDistConf = require('../conf/webpack-dist.conf');
 const gulpConf = require('../conf/gulp.conf');
-<% if (js === 'babel') { -%>
-const env = require('gulp-env');
-<% } -%>
 <% if (framework !== 'react') { -%>
 const browsersync = require('browser-sync');
 <% } -%>
 
 gulp.task('webpack:dev', done => {
-  env.set({BABEL_ENV: 'dev'});
   webpackWrapper(false, webpackConf, done);
 });
 
 gulp.task('webpack:watch', done => {
-  env.set({BABEL_ENV: 'dev'});
   webpackWrapper(true, webpackConf, done);
 });
 
 gulp.task('webpack:dist', done => {
-  env.set({BABEL_ENV: 'production'});
+  process.env.NODE_ENV = 'production';
   webpackWrapper(false, webpackDistConf, done);
 });
 
